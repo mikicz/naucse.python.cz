ddb995fc796284fadbb058491ccc966bede811e8
Mikuláš Poul <mikulaspoul@gmail.com>
2017-11-04 19:55:40 +0100

    Logging arca

--- naucse/__init__.py
+++ naucse/__init__.py
@@ -1,4 +1,7 @@
+import logging
 import sys
+from logging.handlers import RotatingFileHandler
+
 if sys.version_info[0] <3 :
     raise RuntimeError('We love Python 3.')
 
@@ -7,4 +10,13 @@ from naucse.routes import app
 
 
 def main():
+    handler = RotatingFileHandler('.arca/arca.log', maxBytes=10000, backupCount=0)
+    formatter = logging.Formatter("[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
+
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger("arca")
+    logger.addHandler(handler)
+
     cli(app, base_url='http://naucse.python.cz')
